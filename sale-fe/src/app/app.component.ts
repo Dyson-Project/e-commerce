7a7562649560f33abf2996906f3c986812fd985a
longtpt <longtpt@unicloud.com.vn>
2023-05-13 17:53:33 +0700

    [ADD] add fe new source

--- sale-fe/src/app/app.component.ts
+++ sale-fe/src/app/app.component.ts
@@ -1,10 +1,4 @@
-import { Component, ViewContainerRef } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { SecurityService } from './shared/services/security.service';
-import { Configuration } from 'tslint';
-import { ConfigurationService } from './shared/services/configuration.service';
-import { ToastrService } from 'ngx-toastr';
-import { Title } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,25 +6,5 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  Authenticated: boolean = false;
-  subscription!: Subscription;
- constructor(
-   private titleService: Title,
-   private securityService: SecurityService,
-   private configurationService: ConfigurationService,
-   private toastr: ToastrService,
-   vcr: ViewContainerRef
- ) {
-    this.Authenticated = this.securityService.IsAuthorized; 
- }
-
- ngOnInit(): void {
-    console.log('app on init');
-    this.subscription = this.securityService.authenticationChallenge$.subscribe(res => this.Authenticated = res);
-    this.configurationService.load(); 
- }
-
- public setTitle(newTitle: string){
-   this.titleService.setTitle('EShop');
- }
+  title = 'sale-fe';
 }
